c325fc8adaa63c81fe917e5ec073c8cc46627d3a
halukoz <halukozduman@gmail.com>
2017-01-15 00:12:58 +0100

    Bug fixed: key controls are limited with list size and index numbers, such as if index = 0, key up is not worked

--- js/controllers.js
+++ js/controllers.js
@@ -15,11 +15,19 @@ controllers.controller('mainCtrl', ['$scope', '$rootScope','$location', '$http',
   scope.selectedAddressIndex = -1;
 
   scope.searchCountry = function() {
-		scope.countryList = autoCompletionService.search(countryService.getCountryList(),scope.countryText);
+    if (scope.countryText.length === 0) {
+			scope.countryList = [];
+		} else {
+			scope.countryList = autoCompletionService.search(countryService.getCountryList(),scope.countryText);
+		}
   }
 
   scope.searchAddress = function() {
-		scope.addressList =  autoCompletionService.search(scope.currentAddresses,scope.addressText);
+		if(scope.addressText.length === 0) {
+			scope.addressList = [];
+		} else {
+			scope.addressList =  autoCompletionService.search(scope.currentAddresses,scope.addressText);
+		}
   }
 
   addressService.findMyCoordinates().then(function(response) {
@@ -52,24 +60,36 @@ controllers.controller('mainCtrl', ['$scope', '$rootScope','$location', '$http',
 	}
 
   scope.controlCountryListWithKey = function(event) {
-     if (event.keyCode === 40) {
-				scope.selectedCountryIndex++;
-		} else if (event.keyCode === 38) {
-				scope.selectedCountryIndex--;
-    } else if(event.keyCode === 13){
+		if(event.keyCode === 13){
         scope.countryText = scope.countryList[scope.selectedCountryIndex];
         scope.countryList = [];
+				scope.selectedCountryIndex = 0;
 		}
+    if (event.keyCode === 40) {
+			if((scope.selectedCountryIndex < 5) && (scope.selectedCountryIndex+1 < scope.countryList.length)) {
+					scope.selectedCountryIndex++;
+			}
+		} else if (event.keyCode === 38) {
+			if(scope.selectedCountryIndex-1 >= 0) {
+				scope.selectedCountryIndex--;
+			}
+    }
 	}
 
 	scope.controlAddressListWithKey = function(event) {
-     if (event.keyCode === 40) {
-				scope.selectedAddressIndex++;
+		if(event.keyCode === 13){
+			scope.addressText = scope.addressList[scope.selectedAddressIndex];
+			scope.addressList = [];
+			scope.selectedAddressIndex = 0;
+		}
+		 if (event.keyCode === 40) {
+			if((scope.selectedAddressIndex < 5) && (scope.selectedAddressIndex+1 < scope.addressList.length)) {
+					scope.selectedAddressIndex++;
+			}
 		} else if (event.keyCode === 38) {
-				scope.selectedCountryIndex--;
-    } else if(event.keyCode === 13){
-        scope.addressText = scope.addressList[scope.selectedAddressIndex];
-        scope.addressList = [];
+			if(scope.selectedAddressIndex-1 >= 0) {
+				scope.selectedAddressIndex--;
+			}
 		}
 	}
 
