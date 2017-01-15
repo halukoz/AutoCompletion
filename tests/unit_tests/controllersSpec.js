7a03372093b050a90d4cc3dc992dc86cfa7101bf
halukoz <halukozduman@gmail.com>
2017-01-15 02:08:17 +0100

    new tests are added, google api and key events are tested

--- tests/unit_tests/controllersSpec.js
+++ tests/unit_tests/controllersSpec.js
@@ -1,17 +1,17 @@
 describe('Testing Autocompletion Controllers', function() {
 
   var scope, mainCtrl;
-  var addressService;
   var country_Service;
-
+  var autoCompletion_Service;
 
   beforeEach(module('autocompleteApp'));
 
-  beforeEach(inject(function($controller, $rootScope, _countryService_) {
+  beforeEach(inject(function($controller, $rootScope, _countryService_, _autoCompletionService_) {
 
     country_Service = _countryService_;
+    autoCompletion_Service = _autoCompletionService_;
     scope = $rootScope.$new();
-    mainCtrl = $controller('mainCtrl', {$scope:scope}, {countryService:country_Service});
+    mainCtrl = $controller('mainCtrl', {$scope:scope}, {countryService:country_Service}, {autoCompletionService:autoCompletion_Service});
     spyOn(country_Service, 'getCountryList');
   }));
 
@@ -19,8 +19,101 @@ describe('Testing Autocompletion Controllers', function() {
     expect(country_Service.getCountryList).not.toHaveBeenCalled();
   })
 
-  it("Should be address list size = 24", function() {
+  it("Should be address list size = 0, if countryText", function() {
+    scope.countryText = "";
     scope.searchCountry();
-    expect(country_Service.getCountryList).toHaveBeenCalled();
+    expect(country_Service.getCountryList).not.toHaveBeenCalled();
+  });
+
+  it("Select country test in [Germany, France] array", function() {
+    scope.countryList = ["Germany", "France"];
+    scope.selectCountry(0);
+    expect(scope.countryText).toBe("Germany");
+  });
+
+  it("Select address test in [Charl, France] array", function() {
+    scope.addressList = ["Charlottenburg", "Mitte"];
+    scope.selectAddress(1);
+    expect(scope.addressText).toBe("Mitte");
+  });
+
+  it("Key down tests for Address Search Box", function() {
+    scope.selectedAddressIndex = 1;
+    scope.addressList = ["Charlottenburg", "Mitte", "Kreuzberg"];
+    var event = document.createEvent("Events");
+    event.initEvent("keydown", true, true);
+    event.keyCode = 40;
+    scope.controlAddressListWithKey(event);
+    expect(scope.selectedAddressIndex).toBe(2);
+  });
+
+  it("Key up tests for Address Search Box", function() {
+    scope.selectedAddressIndex = 2;
+    scope.addressList = ["Charlottenburg", "Mitte", "Kreuzberg"];
+    var event = document.createEvent("Events");
+    event.initEvent("keydown", true, true);
+    event.keyCode = 38;
+    scope.controlAddressListWithKey(event);
+    expect(scope.selectedAddressIndex).toBe(1);
+  });
+
+  it("Key up tests for Address Search Box, important case, index = 0, result should be 0, otherwise cursor will be disappeared", function() {
+    scope.selectedAddressIndex = 0;
+    scope.addressList = ["Charlottenburg", "Mitte", "Kreuzberg"]
+    var event = document.createEvent("Events");
+    event.initEvent("keydown", true, true);
+    event.keyCode = 38;
+    scope.controlAddressListWithKey(event);
+    expect(scope.selectedAddressIndex).toBe(0);
+  });
+
+  it("Key down tests for Address Search Box, important case, index = 5, result should be 5, otherwise cursor will be disappeared", function() {
+    scope.selectedAddressIndex = 5;
+    scope.addressList = ["Charlottenburg", "Mitte", "Kreuzberg"];
+    var event = document.createEvent("Events");
+    event.initEvent("keydown", true, true);
+    event.keyCode = 40;
+    scope.controlAddressListWithKey(event);
+    expect(scope.selectedAddressIndex).toBe(5);
+  });
+
+  it("Key down tests for Country Search Box", function() {
+    scope.selectedCountryIndex = 1;
+    scope.countryList = ["Germany", "France", "Ireland"];
+    var event = document.createEvent("Events");
+    event.initEvent("keydown", true, true);
+    event.keyCode = 40;
+    scope.controlCountryListWithKey(event);
+    expect(scope.selectedCountryIndex).toBe(2);
+  });
+
+  it("Key up tests for Country Search Box", function() {
+    scope.selectedCountryIndex = 2;
+    scope.CountryList = ["Germany", "France", "Ireland"];
+    var event = document.createEvent("Events");
+    event.initEvent("keydown", true, true);
+    event.keyCode = 38;
+    scope.controlCountryListWithKey(event);
+    expect(scope.selectedCountryIndex).toBe(1);
+  });
+
+  it("Key up tests for Country Search Box, important case, index = 0, result should be 0, otherwise cursor will be disappeared", function() {
+    scope.selectedCountryIndex = 0;
+    scope.countryList = ["Germany", "France", "Ireland"];
+    var event = document.createEvent("Events");
+    event.initEvent("keydown", true, true);
+    event.keyCode = 38;
+    scope.controlCountryListWithKey(event);
+    expect(scope.selectedCountryIndex).toBe(0);
+  });
+
+  it("Key down tests for Country Search Box, important case, index = 5, result should be 5, otherwise cursor will be disappeared", function() {
+    scope.selectedCountryIndex = 5;
+    scope.countryList = ["Germany", "France", "Ireland"];
+    var event = document.createEvent("Events");
+    event.initEvent("keydown", true, true);
+    event.keyCode = 40;
+    scope.controlCountryListWithKey(event);
+    expect(scope.selectedCountryIndex).toBe(5);
   });
 });
--- tests/unit_tests/servicesSpec.js
+++ tests/unit_tests/servicesSpec.js
@@ -3,22 +3,37 @@ describe('Testing Autocompletion Services', function() {
   var scope, mainCtrl;
   var addressService;
   var countryService;
+  var autoCompletionService;
+  var $httpBackend;
 
   beforeEach(module('autocompleteApp'));
 
-  beforeEach(inject(function($controller, $rootScope) {
+  beforeEach(inject(function($controller, $rootScope, _$httpBackend_) {
     scope = $rootScope.$new();
     mainCtrl = $controller('mainCtrl', {$scope:scope});
+    $httpBackend = _$httpBackend_;
   }));
 
-  beforeEach(inject(function(_countryService_) {
+  beforeEach(inject(function(_countryService_, _autoCompletionService_, _addressService_) {
     countryService = _countryService_;
+    autoCompletionService = _autoCompletionService_;
+    addressService = _addressService_;
   }));
-
 
   it("Should be address list size 24: ", function() {
-
     expect(countryService.getCountryList().length).toBe(24);
+  });
+
+  it("Should only return 1 value", function() {
+    var testlist = ["Germany", "France", "Romania"];
+    var testSearchKey = "ger"
+    expect(autoCompletionService.search(testlist, testSearchKey).length).toBe(1);
+  });
+
+  it('Google API address test', function () {
+         $httpBackend.expectGET('https://maps.googleapis.com/maps/api/geocode/json?latlng=52.517082599999995,13.323661099999981&sensor=false').respond('Hi!');
+         var list = addressService.findAddressesWithCoordinates("52.517082599999995","13.323661099999981");
+         $httpBackend.flush();
   });
 
 });
